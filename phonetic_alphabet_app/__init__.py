761351973f50793ffb3cbff0691c35a4ac5c164b
Jacob Lapenna <jacoblapenna@gmail.com>
2021-02-21 19:10:35 -0700

    Working on incorporating more views and game.

--- phonetic_alphabet_app/__init__.py
+++ phonetic_alphabet_app/__init__.py
@@ -2,6 +2,5 @@ from flask import Flask
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
-socket = SocketIO(app)
 
 import phonetic_alphabet_app.views
--- phonetic_alphabet_app/views.py
+++ phonetic_alphabet_app/views.py
@@ -1,6 +1,24 @@
 from phonetic_alphabet_app import app
-from flask import render_template
+from flask import render_template, url_for, redirect, abort
+from flask_socketio import SocketIO
+
+socket = SocketIO(app)
 
 @app.route('/')
-def index():
+def landing():
     return render_template("landing.html")
+
+@app.route("/quiz/<order>")
+def serve_game(order):
+    print(order)
+    if order == "alphabetical":
+        return render_template("game.html")
+    if order == "random":
+        return render_template("game.html")
+    else:
+        return abort(404)
+
+@socket.on("start_quiz")
+def order_selected(quiz_order):
+    print(url_for("serve_game", order=quiz_order))
+    return redirect(url_for("serve_game", order=quiz_order))
